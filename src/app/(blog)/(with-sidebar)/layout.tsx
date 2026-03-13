f4eea5fd6dc39b9d3bb1850c134957bd32ac44e7
mulgko <uiissoop@gmail.com>
2026-03-13 16:43:49 +0900

    feat: 메인페이지와 상세페이지 사이드바 분리

--- src/app/(blog)/(with-sidebar)/layout.tsx
+++ src/app/(blog)/(with-sidebar)/layout.tsx
@@ -1,17 +1,18 @@
 import React from "react";
-import Header from "../components/common/Header";
-import Footer from "../components/common/Footer";
-import Sidebar from "../components/common/Sidebar";
-import { siteConfig } from "../lib/config";
+import Header from "@/src/app/components/common/Header";
+import Footer from "@/src/app/components/common/Footer";
+import Sidebar from "@/src/app/components/common/Sidebar";
+import { siteConfig } from "@/src/app/lib/config";
 
 const layout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div className="min-h-screen flex flex-col gap-10 overflow-x-hidden">
-      <Header />
       <div className="flex-1 max-w-5xl mx-auto px-4 w-full flex gap-10">
         <main className="flex-1 min-w-0">{children}</main>
+        <div className="w-46 shrink-0 hidden lg:block">
+          <Sidebar tags={siteConfig.sidebarTags} />
+        </div>
       </div>
-      <Footer />
     </div>
   );
 };
